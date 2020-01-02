1974e8c1d6bb8772f6730a40fdf4ac17c0b74df7
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-02 00:31:29 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,6 @@
+// Assign an object to the variable playlist and initialize the object with a key-value pair
+// — the keys will be artist names and the values will be song titles.
+
+
+var playlist = new Object();
+playlist = { artist: 'song'};
