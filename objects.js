6377133377dde8abd19e38a9af5a2969b2831a1a
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-02 00:42:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -10,6 +10,10 @@ playlist = { artist: 'song'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
-
   return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist
 }
