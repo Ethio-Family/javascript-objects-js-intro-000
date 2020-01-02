266ad7427f1942db6cf4dae8112331b0567d9280
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-02 00:37:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ playlist = { artist: 'song'};
 // The body of the function should add the song and artist as a key-value pair to the playlist object.
 // The function should return the whole playlist.
 
-function updatePlaylist(playlist, artist, 'song' ) {
+function updatePlaylist(playlist, key, value ) {
   return playlist;
 }
